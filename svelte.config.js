0b954bdb49d9216b705d4825c2da10c78831aeb7
opasnostinet <veselovalova@icloud.com>
2021-11-14 16:35:41 +0300

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+// for more information about preprocessors
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	preprocess: preprocess({}),
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+	},
 };
 
 export default config;
